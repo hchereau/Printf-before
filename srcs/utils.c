a306ff1c6044ed946b18541a77653895ab26ee98
Hugo CHEREAU <hchereau@e1r2p14.clusters.42paris.fr>
2023-02-04 17:47:29 +0100

    oui

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alpha.c                                   :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hchereau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/04 11:02:32 by hchereau          #+#    #+#             */
-/*   Updated: 2023/02/04 16:48:08 by hchereau         ###   ########.fr       */
+/*   Created: 2023/02/04 16:01:27 by hchereau          #+#    #+#             */
+/*   Updated: 2023/02/04 16:48:04 by hchereau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		get_arg_c(char	**buffer, int c)
+int		add_buffer(char **buffer, char *src, size_t	index_buffer)
 {
-	
+	size_t	len_src = ft_strlen(src);
+
+	if (len_src + index_buffer < BUFFER_SIZE)
+	{
+		ft_strlcpy(buffer, src, len_src);
+		return (1);
+	}
+	return (0);
 }
