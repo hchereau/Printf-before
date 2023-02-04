0c71faa78d811f53c79cfba530b9c07f17cb2d73
Hugo CHEREAU <hchereau@e1r2p14.clusters.42paris.fr>
2023-02-04 12:27:19 +0100

    print_c

---
+++ includes/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchereau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 11:03:25 by hchereau          #+#    #+#             */
+/*   Updated: 2023/02/04 11:55:11 by hchereau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+
+int		ft_printf(const char *, ...);
+int		how_printabl(car);
+int		get_arg_c(char	**buffer, va_list **arg);
+
+#endif
