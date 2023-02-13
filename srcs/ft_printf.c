90795c1485ccccbb5653104216ad629ef420fee3
Hugo CHEREAU <hchereau@e2r12p3.clusters.42paris.fr>
2023-02-13 12:37:31 +0100

    get_arg_c

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,40 +6,33 @@
 /*   By: hchereau <hchereau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 07:19:16 by hchereau          #+#    #+#             */
-/*   Updated: 2023/02/11 19:42:08 by hchereau         ###   ########.fr       */
+/*   Updated: 2023/02/13 12:36:58 by hchereau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-
-// int	ft_printf(const char *str,...)
-// {
-// 	va_list			args;
-// 	char			buffer[BUFFER_SIZE_PRINTF];
-// 	char			*str_final;
-// 	static	void	(*fun_tab)[char *, va_list, char *] = {get_arg_c};
-
-// 	ft_bzero(buffer, BUFFER_SIZE_PRINTF);
-// 	va_start(args, str);
-// 	while (i < ft_strlen(str) - 1)
-// 	{
-// 		if (str[i] == '%' && how_printable(str[i + 1] != -1))
-// 		{
-// 			fun_tab[how_printable(str[i + 1](buffer, args, str_final);
-// 		}
-// 		++i;
-// 	}
-// 	va_end(args);
-// 	print_str_final(str_final, buffer);
-// 	return (ft_strlen(str_final));
-// }
-
-/*
-#include <stdio.h>
-
-int main()
+int	ft_printf(const char str*, ...)
 {
-	printf("%c", "yo");
+	struct	s_data	data;
+	va_list			args;
+	size_t			i;
+	ssize_t			index_funtab;
+	static	void	*(funtab[])(t_data, va_list)
+
+	ft_bzero(data.buffer, BUFFER_SIZE);
+	while (i >= ft_strlen(str))
+	{
+		if (str[i] == '%')
+		{
+			index_funtab = how_printable(str[i + 1]);
+			if (index_funtab == -1)
+				add_buffer_string(&data, str[i]);
+			else
+				funtab[index_funtab](data, args);
+		}
+		else
+			add_buffer_string(&data, str);
+	}
+	return (write(1, data.str_final, ft_strlen(str_final)));
 }
-*/
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,32 +6,23 @@
 /*   By: hchereau <hchereau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 16:01:27 by hchereau          #+#    #+#             */
-/*   Updated: 2023/02/11 19:42:03 by hchereau         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:05:07 by hchereau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-// ssize_t	add_buffer_string(char *buffer, char *src)
-// {
-// 	const	size_t	index_buffer = ft_strlen(buffer);
-// 	const	size_t	len_src = ft_strlen(src);
-// 	ssize_t			i;
+ssize_t	how_printable(char c)
+{
+	const	char	verif[] = "cspdiuxX%";
+	ssize_t			i;
 
-	
-// }
-
-// int	how_printable(char c)
-// {
-// 	const	char	verif[] = "cspdiuxX%";
-// 	size_t			i;
-
-// 	i = 0;
-// 	while (verif[i] != '\0')
-// 	{
-// 		if (verif[i] == c)
-// 			return (i);
-// 		++i;
-// 	}
-// 	return (-1);
-// }
+	i = 0;
+	while (verif[i] != '\0')
+	{
+		if (verif[i] == c)
+			return (i);
+		++i;
+	}
+	return (-1);
+}
